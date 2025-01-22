af032f964fe9c7178bf7ae881d7af185f360fbf1
nishchay <nishchaynilekani@gmail.com>
2025-01-22 11:07:07 +0530

    First Commit

---
+++ SpringbootShoppingApp/src/main/java/com/cts/demo/SpringbootShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.cts.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootShoppingAppApplication.class, args);
+	}
+
+}
